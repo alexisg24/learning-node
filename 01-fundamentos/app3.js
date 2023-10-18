83350a87dc700170a61b64736f4f5576fe994ab4
Alexis Garcia <azoreno2@gmail.com>
2023-10-17 21:57:16 -0400

    update (01-Fundamentos): Fix typos

--- 01-fundamentos/app3.js
+++ 01-fundamentos/app3.js
@@ -6,4 +6,4 @@ const wordCount = content.split(' ')
 const reactCount = wordCount.filter(text => text.toLocaleLowerCase().includes('react'))
 
 console.log('Palabras totales: ', wordCount.length)
-console.log('React totales: ', reactCount.length)
+console.log('React palabras totales: ', reactCount.length)
